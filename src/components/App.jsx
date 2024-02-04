a583145b800ee1317b3c387f7f89aae2140d5e7d
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2024-02-04 18:39:53 +0200

    main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,70 @@
-import MyPhoneBook from "./MyPhone/MyPhoneBook";
+import { useState, useEffect } from 'react';
 
+import { nanoid } from 'nanoid';
+
+import PhoneForm from './MyPhone/PhoneForm/PhoneForm';
+
+import PhoneList from './MyPhone/PhoneList/PhoneList';
 
 export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const data = JSON.parse(localStorage.getItem('myPhoneBook'));
+    return data || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('myPhoneBook', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const [filter, setFilter] = useState('');
+
+  const isDulecate = ({ name }) => {
+    const normalazeName = name.toLowerCase();
+    const dublicate = contacts.find(item => {
+      const normalizedCurrentName = item.name.toLowerCase();
+      return normalazeName === normalizedCurrentName;
+    });
+    return Boolean(dublicate);
+  };
+
+  const addForPhenebook = data => {
+    if (isDulecate(data)) {
+      return alert(`Name {data.name} and ${data.number} already in Phonebook`);
+    }
+    setContacts(prevContacts => {
+      const newContact = { id: nanoid(), ...data };
+      return [...contacts, newContact];
+    });
+  };
+
+  const deleteName = id => {
+    setContacts(prevContacts => prevContacts.filter(item => item.id !== id));
+  };
+  const handelSearce = ({ target }) => {
+    setFilter(target.value);
+  };
+  const getFilerName = () => {
+    if (!filter) {
+      return contacts;
+    }
+    const normalazeNameFilter = contacts.filter.toLowerCase();
+    const filterName = contacts.filter(({ name }) => {
+      const filterCurentName = name.toLowerCase();
+      return filterCurentName.includes(normalazeNameFilter);
+    });
+    return filterName;
+  };
+
+  const items = getFilerName();
   return (
-    <div    >
-     <MyPhoneBook /> 
+    <div>
+      <PhoneForm onSubmit={addForPhenebook} />
+
+      <PhoneList
+        items={items}
+        deleteName={deleteName}
+        handelSearce={handelSearce}
+      />
     </div>
   );
 };
--- src/components/MyPhone/PhoneForm/PhoneForm.jsx
+++ src/components/MyPhone/PhoneForm/PhoneForm.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import css from './PhoneForm.module.css';
 
@@ -9,69 +9,61 @@ const INITIAL_STATE = {
   number: '',
 };
 
-class PhoneForm extends Component {
-  state = { ...INITIAL_STATE };
-  nameId = nanoid();
-  numbId = nanoid();
+const PhoneForm = ({ onSubmit }) => {
+  const [state, setState] = useState({ ...INITIAL_STATE });
+  const nameId = nanoid();
+  const numbId = nanoid();
 
-  addForPhenebook = () => {};
-
-  handelChange = ({ target }) => {
+  const handelChange = ({ target }) => {
     const { name, value } = target;
-    this.setState({
-      [name]: value,
-    });
+    setState({ ...state, [name]: value });
   };
 
-  handelSubmit = e => {
+  const handelSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit({ ...this.state });
-    this.setState({ ...INITIAL_STATE });
+    onSubmit({ ...state });
+    setState({ ...INITIAL_STATE });
   };
+  const { name, number } = state;
+  return (
+    <form onSubmit={handelSubmit} className={css.form}>
+      <h1 className={css.titel}>Phonebook</h1>
+      <div>
+        <label htmlFor={nameId}>Name</label>
+        <input
+          pattern="^[a-zA-Zа-яА-Я]+(([' \\\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          value={name}
+          onChange={handelChange}
+          className={css.input}
+          id={nameId}
+          type="text"
+          name="name"
+          required
+          placeholder="Enter you name "
+        ></input>
+      </div>
 
-  render() {
-    const { name, number } = this.state;
-    const { nameId, numbId, handelSubmit, handelChange } = this;
-    return (
-      <form onSubmit={handelSubmit} className={css.form}>
-        <h1 className={css.titel}>Phonebook</h1>
-        <div>
-          <label htmlFor={nameId}>Name</label>
-          <input
-            pattern="^[a-zA-Zа-яА-Я]+(([' \\\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            value={name}
-            onChange={handelChange}
-            className={css.input}
-            id={nameId}
-            type="text"
-            name="name"
-            required
-            placeholder="Enter you name "
-          ></input>
-        </div>
+      <div>
+        <label htmlFor={numbId}>Number </label>
+        <input
+          pattern="(\+?\d{1,4}[ \-]?)?(\(?\d{1,3}\)?[ \-]?)?\d{1,4}[ \-]?\d{1,4}[ \-]?\d{1,9}"
+          title="Enter the correct phone number"
+          value={number}
+          onChange={handelChange}
+          className={css.input}
+          id={numbId}
+          type="tel"
+          name="number"
+          required
+          placeholder="Enter you number "
+        ></input>
+      </div>
 
-        <div>
-          <label htmlFor={numbId}>Number </label>
-          <input
-            pattern="(\+?\d{1,4}[ \-]?)?(\(?\d{1,3}\)?[ \-]?)?\d{1,4}[ \-]?\d{1,4}[ \-]?\d{1,9}"
-            title="Enter the correct phone number"
-            value={number}
-            onChange={handelChange}
-            className={css.input}
-            id={numbId}
-            type="tel"
-            name="number"
-            required
-            placeholder="Enter you number "
-          ></input>
-        </div>
-
-        <button className={css.btn} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
-  }
-}
+      <button className={css.btn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
 
 export default PhoneForm;
